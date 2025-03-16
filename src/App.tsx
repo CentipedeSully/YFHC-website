bf845bc6d840bbb7f341732775ca52433df3d512
William Sullivan-Smith <sullivansmith057@gmail.com>
2025-03-15 21:27:04 -0500

    initialized react app with vite and tailwind.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className='bg-red-500'>
+      <p>wadda!</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}", // Add your project's file extensions and paths here
+    ],
+    theme: {
+        extend: {
+        // Add your custom theme configurations here
+        },
+    },
+    plugins: [],
+};
