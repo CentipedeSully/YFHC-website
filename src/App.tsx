feaae0edd97c37d48173f6f0df680419e123be5b
William Sullivan-Smith <sullivansmith057@gmail.com>
2025-03-18 19:08:40 -0500

    added more content. Redesigned to be simpler and less ugly.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       </div>
 
       <nav id="navbar" className="bg-logo-purple h-10 flex flex-row px-10 justify-center py-1 space-x-1.5 fonted">
-        <span className="px-3 text-gray-200 rounded my-auto py-1 mr-10">- Home Cleaning Service -</span>
+        <span className="px-3 text-gray-200 rounded my-auto py-1 mr-10">- Kansas City Home Cleaning Service -</span>
         < NavButton
           scrollTo={"#welcome-section"}
           text={"About"}
@@ -33,26 +33,200 @@ function App() {
 
       </nav>
 
-      <div id="welcome-section" className="mx-10">
+      <div id="welcome-section" className="mx-20 bg-white">
 
-        <div className="bg-internal-welcome w-full relative h-125">
-          <div className="text-center text-logo-purple rounded absolute top-5 right-30 w-85">
-            <p className="text-3xl px-5 bg-white rounded-t pb-2">
-              A <span className=" text-4xl italic">Clean</span> Home <br/>is a <span className=" text-4xl italic">Happy</span> Home
-            </p>
-            <p className="py-3 opacity-75 bg-white rounded-b">We are a licensed and insured, family-owned, home-cleaning business that values fostering positivity, deomstrating accountability, and providing quality service to our clients.</p>
+        <div className="flex flex-row w-full justify-center space-x-1 relative">
+          <div className="absolute w-full h-full text-white text-center">
+            <div className="flex flex-col justify-center h-full">
+              <div className="mb-5">
+                <p className="text-7xl font-bold drop-shadow-lg">Clean homes are happy homes</p>
+                <p className="text-2xl pt-2 font-bold drop-shadow-lg">Providing service for the Greater Kansas City area</p>
+              </div>
+              
+              <a 
+                className="px-4 py-1 bg-logo-purple rounded mx-auto hover:bg-rose-950"
+                href="#contact-section">Schedule A Cleaning Today
+              </a>
+            </div>
+          </div>
+
+          <img 
+            className="object-cover w-1/2" 
+            src="./src/assets/pictures/IMG-20250312-WA0005.jpg" 
+            alt="missing" 
+          />
+
+          <div className="flex flex-col space-y-1">
+            <img 
+              className="object-cover h-1/2" 
+              src="./src/assets/pictures/IMG-20250312-WA0018.jpg" 
+              alt="missing" />
+            <img 
+              className="object-cover h-1/2" 
+              src="./src/assets/pictures/IMG-20250312-WA0002.jpg" 
+              alt="missing" />
           </div>
           
         </div>
-      </div>
+        
+        
+        <div className="text-black mt-10 ">
+          <p className="text-6xl font-bold ml-20">Who Are We</p>
+          <div className="flex flex-row h-120 pb-10">
+            <div className="flex flex-col pt-5 px-4 w-1/2">
+              <p className="text-3xl "> We are a licensed and insured, 
+                family-owned home cleaning business that values fostering positivity,
+                demonstrating accountability, and providing quality service to our clients.
+              </p>
 
-      <hr className="border-logo-purple "/>
-
-      <div id="service-section" className="bg-gray-300 mx-10">
-        <div className="flex flex-row ">
+              <a 
+                className="text-white px-4 py-1 mt-5 bg-logo-purple rounded ml-35 mr-auto hover:bg-rose-950"
+                href="#contact-section">Learn More About Us
+              </a>
+            </div>
+            <img 
+              className="h-full object-contain rounded "
+              src="./src/assets/pictures/IMG-20250315-WA0018.jpg" 
+              alt="" />
+          </div>
+          
 
         </div>
+        
       </div>
+
+
+
+      <div id="service-section" className=" bg-gray-300">
+        <div className="mx-20 py-4 text-black ">
+          <div className="flex flex-row-reverse">
+            <p className="w-1/2 font-bold text-6xl text-center pb-5">Our Services</p>
+          </div>
+          
+          <div className="flex flex-row-reverse text-black space-x-1 h-80 pb-1">
+
+            <div className="w-1/2 px-5">
+              <p className="text-3xl" >Our offered services include (but aren't limited to):</p>
+              <ul className="text-3xl pl-5">
+                <li>- Routine Cleaning</li>
+                <li>- Deep Cleaning</li>
+                <li>- Office Cleaning</li>
+                <li>- New Construction Cleaning</li>
+                <li>- And More!</li>
+              </ul>
+            </div>
+
+            <div className="w-1/2 ">
+              <div id="first-services-caro" className="carousel h-full">
+                <CarouselItem 
+                  itemId="first-services-caro-1"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0000.jpg"
+                  prevID="first-services-caro-3"
+                  nextId="first-services-caro-2"
+                />
+
+                <CarouselItem 
+                  itemId="first-services-caro-2"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0003.jpg"
+                  prevID="first-services-caro-1"
+                  nextId="first-services-caro-3"
+                />
+
+                <CarouselItem 
+                  itemId="first-services-caro-3"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0004.jpg"
+                  prevID="first-services-caro-2"
+                  nextId="first-services-caro-1"
+                />
+              </div>
+            </div>
+
+          </div>
+
+          <div className="flex flex-row h-80 space-x-1">
+
+            <div className="w-1/2 h-80">
+              <div id="sec-services-caro" className="carousel h-full">
+                <CarouselItem 
+                  itemId="sec-services-caro-1"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0006.jpg"
+                  prevID="sec-services-caro-"
+                  nextId="sec-services-caro-2"
+                />
+
+                <CarouselItem 
+                  itemId="sec-services-caro-2"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0007.jpg"
+                  prevID="sec-services-caro-1"
+                  nextId="sec-services-caro-3"
+                />
+
+                <CarouselItem 
+                  itemId="sec-services-caro-3"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0008.jpg"
+                  prevID="sec-services-caro-2"
+                  nextId="sec-services-caro-4"
+                />
+
+                <CarouselItem 
+                  itemId="sec-services-caro-4"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0010.jpg"
+                  prevID="sec-services-caro-3"
+                  nextId="sec-services-caro-5"
+                />
+
+                <CarouselItem 
+                  itemId="sec-services-caro-5"
+                  imgUrl="./src/assets/pictures/IMG-20250315-WA0005.jpg"
+                  prevID="sec-services-caro-4"
+                  nextId="sec-services-caro-6"
+                />
+
+                <CarouselItem 
+                  itemId="sec-services-caro-6"
+                  imgUrl="./src/assets/pictures/IMG-20250315-WA0008.jpg"
+                  prevID="sec-services-caro-5"
+                  nextId="sec-services-caro-1"
+                />
+              </div>
+            </div>
+
+            <div className="w-1/2">
+            <div id="third-services-caro" className="carousel h-full">
+                <CarouselItem 
+                  itemId="third-services-caro-1"
+                  imgUrl="./src/assets/pictures/IMG-20250315-WA0009.jpg"
+                  prevID="third-services-caro-3"
+                  nextId="third-services-caro-2"
+                />
+
+                <CarouselItem 
+                  itemId="third-services-caro-2"
+                  imgUrl="./src/assets/pictures/IMG-20250315-WA0013.jpg"
+                  prevID="third-services-caro-1"
+                  nextId="third-services-caro-3"
+                />
+
+                <CarouselItem 
+                  itemId="third-services-caro-3"
+                  imgUrl="./src/assets/pictures/IMG-20250312-WA0009.jpg"
+                  prevID="third-services-caro-2"
+                  nextId="third-services-caro-1"
+                />
+
+              </div>
+              
+            </div>
+          </div>
+
+        </div>
+
+        
+
+        
+      </div>
+
+
 
       <div id="contact-section" className="h-100 mx-10 flex flex-col justify-center">
 
@@ -119,8 +293,6 @@ interface buttonProps{
 
 const NavButton = (props:buttonProps) =>{
 
-
-
   return (
     <a 
       className="hover:bg-rose-950 px-3 text-gray-200 rounded my-auto py-1"
@@ -133,63 +305,27 @@ const NavButton = (props:buttonProps) =>{
 }
 
 
-const ServiceCarousel = () =>{
 
-  const componentId = "service-carousel"
-
+interface caroProps{
+  itemId:string,
+  prevID:string,
+  nextId:string,
+  imgUrl:string,
+}
+const CarouselItem = (props:caroProps) =>{
+  
   return(
-    <div id={componentId} className="carousel">
-      <div id={componentId + "-1"} className="carousel-item relative w-full">
-        <img className="" src='./src/assets/pictures/IMG-20250315-WA0009.jpg' alt="missed" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={"#" + componentId + "-3"} className="btn btn-circle touch-pan-x">❮</a>
-          <a href={"#" + componentId + "-2"} className="btn btn-circle touch-pan-x">❯</a>
-        </div>
-      </div>
-      <div id={componentId + "-2"} className="carousel-item relative w-full">
-        <img className="" src="./src/assets/pictures/IMG-20250315-WA0010.jpg" alt="missed" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={"#" + componentId + "-1"} className="btn btn-circle touch-pan-x">❮</a>
-          <a href={"#" + componentId + "-3"} className="btn btn-circle touch-pan-x">❯</a>
-        </div>
-      </div>
-      <div id={componentId + "-3"} className="carousel-item relative w-full">
-        <img className="" src="./src/assets/pictures/IMG-20250315-WA0005.jpg" alt="missed" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={"#" + componentId + "-2"} className="btn btn-circle touch-pan-x">❮</a>
-          <a href={"#" + componentId + "-1"} className="btn btn-circle touch-pan-x">❯</a>
-        </div>
+    <div id={props.itemId} className={"carousel-item relative w-full h-full"}>
+      <img className="rounded mx-auto object-contain" src={props.imgUrl} alt="missed" />
+      <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+        <a href={"#" +props.prevID} className="btn btn-circle">❮</a>
+        <a href={"#" +props.nextId} className="btn btn-circle">❯</a>
       </div>
     </div>
   )
+
+
+
 }
 
-const WelcomeCarousel = () =>{
-  const componentId = "welcome-carousel"
 
-  return(
-    <div id={componentId} className="carousel mx-20 h-125">
-      <div id={componentId + "-1"} className="carousel-item relative contain-content">
-        <img className="" src='./src/assets/pictures/IMG-20250312-WA0018.jpg' alt="missed" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={"#" + componentId + "-3"} className="btn btn-circle touch-pan-x">❮</a>
-          <a href={"#" + componentId + "-2"} className="btn btn-circle touch-pan-x">❯</a>
-        </div>
-      </div>
-      <div id={componentId + "-2"} className="carousel-item relative contain-content">
-        <img className="" src="./src/assets/pictures/IMG-20250312-WA0003.jpg" alt="missed" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={"#" + componentId + "-1"} className="btn btn-circle touch-pan-x">❮</a>
-          <a href={"#" + componentId + "-3"} className="btn btn-circle touch-pan-x">❯</a>
-        </div>
-      </div>
-      <div id={componentId + "-3"} className="carousel-item relative contain-content">
-        <img className="" src="./src/assets/pictures/IMG-20250312-WA0005.jpg" alt="missed" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={"#" + componentId + "-2"} className="btn btn-circle touch-pan-x">❮</a>
-          <a href={"#" + componentId + "-1"} className="btn btn-circle touch-pan-x">❯</a>
-        </div>
-      </div>
-    </div>
-  )
-}
