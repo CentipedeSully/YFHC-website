1cb4b10ef4821ffaf23d1c0f9e10d483770c50ef
William Sullivan-Smith <sullivansmith057@gmail.com>
2025-08-14 23:26:02 -0500

    ReInitialized project without typescript bloat. Now I know what I'm doing

---
+++ YFHC_V2/src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-900'>
+        HelloWorld
+      </div>
+    </>
+  )
+}
+
+export default App
