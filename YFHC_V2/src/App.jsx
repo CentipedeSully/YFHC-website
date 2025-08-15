cc4be2e4545b91babf9a0036763a8d14d9f0fff6
William Sullivan-Smith <sullivansmith057@gmail.com>
2025-08-15 13:34:26 -0500

    blocked out main sections and defined colors

--- YFHC_V2/src/App.jsx
+++ YFHC_V2/src/App.jsx
@@ -5,11 +5,62 @@ function App() {
 
   return (
     <>
-      <div className='bg-amber-900'>
-        HelloWorld
+      <div className='flex flex-col h-screen'>
+        <Header />
+        <LandingArea />
+        <ServicesArea />
+        <GalleryArea />
+        <Footer />
       </div>
     </>
   )
 }
 
 export default App
+
+
+
+
+
+export function Header(){
+
+
+
+
+  return (
+    <div className='h-4 bg-purpleAccent'>
+    </div>
+  )
+}
+
+export function LandingArea(){
+  return (
+    <div className='flex flex-col justify-center flex-1 bg-white'>
+      <p className='text-center'>landing</p>
+    </div>
+  )
+}
+
+export function ServicesArea(){
+  return (
+    <div className='flex flex-col justify-center flex-1 bg-salmonBg '>
+      <p className='text-center'>services</p>
+    </div>
+  )
+}
+
+export function GalleryArea(){
+  return (
+    <div className='flex flex-col justify-center flex-1 '>
+      <p className='text-center'>gallery</p>
+    </div>
+  )
+}
+
+export function Footer(){
+
+  return(
+    <div className='h-4 bg-purpleAccent'>
+    </div>
+  )
+}
