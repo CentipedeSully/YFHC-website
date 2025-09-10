73b6ea2f73e43fe31dcd0e3e9d7ae30e3e111bbd
William Sullivan-Smith <sullivansmith057@gmail.com>
2025-09-10 12:54:30 -0500

    filled out services text and revamped services design. Added another floating panel to ease the transition from services to howTo. filled out new panel.

--- YFHC_V2/src/App.jsx
+++ YFHC_V2/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className='text-lg'>
+      <div className='text-lg bg-pink-1 w-150  sm:w-full mx-auto'>
         <Header />
         <LandingArea />
         <ServicesArea />
@@ -36,10 +36,10 @@ export function Header(){
 
 export function LandingArea(){
   return (
-    <div className=' w-full'>
+    <div className=' w-150 sm:w-full'>
       <div className=' bg-pink-1 h-140 lg:h-160 opacity-90 w-full relative overflow-hidden'>
-        <div className='absolute -z-10  w-full h-full flex justify-center'>
-          <img className='object-cover w-full absolute -top-42 sm:-top-90 md:hidden' src="./src/landing_mobile.jpg" alt="pillow_stack" />
+        <div className='absolute -z-10  w-150 sm:w-full h-full flex justify-center'>
+          <img className='object-cover w-150 sm:w-full absolute -top-42 sm:-top-90 md:hidden' src="./src/landing_mobile.jpg" alt="pillow_stack" />
           <img className='object-cover hidden md:block' src='./src/landing_wide_xl.jpg' alt='clean_kitchen'/>
         </div>
         
@@ -105,43 +105,102 @@ export function TestamonialsArea(){
 
 export function ServicesArea(){
   return (
-    <div className=' bg-pink-100 pt-15 lg:pt-60 pb-40 text-pink-7'>
+    <div className=' bg-pink-100 pt-15 w-150 sm:w-full lg:pt-60 text-pink-7'>
       <img className='h-10 object-contain mx-auto mb-5' src="./src/yfhc_logo.png" alt="brand_logo_img" />
       <h2 className='text-xl text-center pb-15'>Our Services</h2>
-      <ul className=' text-pink-5 flex flex-row justify-center gap-15 flex-wrap mx-auto w-3/4'>
-        <li className=' flex flex-col justify-center gap-5'>
-          <h3 className='text-center '>Residence Cleaning</h3>
-          <hr className='w-100 mx-auto '/>
-          <img className='h-80 w-100 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400 ' src="./src/residential.jpeg" alt="living_room_with_vaccuum" />
-          
+      <ul className=' text-pink-5 flex flex-col justify-center gap-10 mx-auto w-150'>
+        <li className='relative flex flex-col justify-center gap-4 w-full'>
+          <h3 className='text-start px-15'>Residential Cleaning</h3>
+          <hr className='w-150 mx-auto '/>
+          <img className=' w-150 h-60 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400 ' src="./src/residential.jpeg" alt="living_room_with_vaccuum" />
+          <div className='w-150 h-60 bg-pink-7 opacity-60 absolute top-15'>
+            
+          </div>
+          <p className='absolute px-5 py-2 w-150 h-60 top-15 text-white'>We provide both deep cleaning and maintanance cleaning services to personal and communal living spaces. These spaces include homes, apartments, shelters, community centers, churches,
+            and more.
+            <br />
+            <br />
+            Our deep and maintainence cleanings target all of the expected surfaces, rooms, and appliances, including bathrooms, living rooms, basements, bedrooms, and kitchens.
+            Simply let us know what you'd like us to target and we'll handle the rest.
+          </p>
         </li>
-        <li className=' flex flex-col justify-center gap-5'>
-          <h3 className='text-center'>Post-Renovation Cleaning</h3>
-          <hr className='w-100 mx-auto '/>
-          <img className='h-80 w-100 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400' src="./src/renovation.jpeg" alt="vaccuum_in_room_during_renovations" />
-          
+
+        <li className='relative flex flex-col justify-center gap-4 w-full'>
+          <h3 className='text-start px-15'>Post-Renovation Cleaning</h3>
+          <hr className='w-150 mx-auto '/>
+          <img className=' w-150 h-60 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400' src="./src/renovation.jpeg" alt="vaccuum_in_room_during_renovations" />
+          <div className='w-150 h-60 bg-pink-7 opacity-75 absolute top-15'>
+            
+          </div>
+          <p className='absolute px-5 py-2 w-150 h-60 top-15 text-white'>We're also qualified to cleanup after large renovations projects, which entail a full top-to-bottom deep cleaning to ensure 
+            all dust, heavy particles, and construction debris is removed from the area.
+            <br />
+            <br />
+            If there are any additional specifics that you'd like us to include, let us and our technicians know so we're better able to meet your needs. 
+          </p>
         </li>
-        <li className=' flex flex-col justify-center gap-5'>
-          <h3 className='text-center'>Workplace Cleaning</h3>
-          <hr className='w-100 mx-auto '/>
-          <img className='h-80 w-100 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400' src="./src/office.jpeg" alt="office_room" />
-          
+
+        <li className='relative flex flex-col justify-center gap-4 w-full'>
+          <h3 className='text-start px-15'>Workplace Cleaning</h3>
+          <hr className='w-150 mx-auto '/>
+          <img className=' w-150 h-60 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400 ' src="./src/office.jpeg" alt="office_room" />
+          <div className='w-150 h-60 bg-pink-7 opacity-75 absolute top-15'>
+            
+          </div>
+          <p className='absolute px-5 py-2 w-150 h-60 top-15 text-white'>Our deep cleaning and maintainence cleaning services also extend to commerical and business surfaces and workspaces.
+            <br />
+            <br />
+            Be sure to communicate the specifics of what to touch and what <span className='italic'>not</span> to touch before the cleaning commenses to help prevent unintended loss of work.
+          </p>
         </li>
-        <li className=' flex flex-col justify-center gap-5'>
-          <h3 className='text-center'>Move Out Cleaning <span className='hidden'>Cleaning</span></h3>
-          <hr className='w-100 mx-auto '/>
-          <img className='h-80 w-100 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400' src="./src/boxes.jpeg" alt="move_out_boxes_in_home" />
-          
+
+        <li className='relative flex flex-col justify-center gap-4 w-full'>
+          <h3 className='text-start px-15'>Move Out Cleaning</h3>
+          <hr className='w-150 mx-auto '/>
+          <img className=' w-150 h-60 opacity-95 object-cover mx-auto shadow border-b-2 border-b-gray-400 ' src="./src/boxes.jpeg" alt="move_out_boxes_in_home" />
+          <div className='w-150 h-60 bg-pink-7 opacity-75 absolute top-15'>
+            
+          </div>
+          <p className='absolute px-5 py-2 w-150 h-60 top-15 text-white'>Whether you're a renter or landlord, we can also handle the cleanup after a big move. We'll provide our deep cleaning 
+            services where it's necessary, and also help haul away any garbage, abandoned furniture and appliances to help save you time and energy.   
+            <br />
+            <br />
+            Once the cleanup is completed, the living space will be clear, clean, and open for reuse.
+          </p>
         </li>
         
       </ul>
 
-      <div className='mt-25 w-100 flex flex-row gap-5 justify-center bg-pink-7 mx-auto px-5 py-3 text-pink-50 rounded'>
+      <div className='mt-15 w-100 flex flex-row gap-5 justify-center bg-pink-7 mx-auto px-5 py-3 text-pink-50 rounded'>
         <span className=''>Request your estimate :</span>
         <span className=''>(913)-230-9208</span>
       </div>
       
       
+      <div className='relative hidden lg:flex justify-center mt-50'>
+        <LicensedAndInsured />
+      </div>
+    </div>
+
+    
+  )
+}
+
+export function LicensedAndInsured(){
+  return(
+    <div className='bg-white z-10 shadow-2xl w-100 lg:w-180 h-65 absolute -translate-y-1/2 rounded'>
+      <div className=''>
+        <h2 className='text-center pt-10 pb-4 text-pink-9'>Licensed And Insured Technicians</h2>
+        <hr className=' border border-pink-3 sm:w-140 mx-auto mb-8'/>
+        <ul className='flex flex-row gap-20 justify-center'>
+          <li>
+            <img className='object-cover w-18' src="./src/award.jpg" alt="award_ribbon" />
+          </li>
+          <li>
+            <p className='w-40 h-full flex text-center items-center pb-3'>With over 6 years of industry experience</p>
+          </li>
+        </ul>
+      </div>
       
     </div>
   )
@@ -149,7 +208,7 @@ export function ServicesArea(){
 
 export function HowItWorks(){
   return (
-    <div className=' bg-white pt-15 pb-70 text-pink-7 text-center '>
+    <div className=' bg-white pt-55 pb-70 w-150 sm:w-full text-pink-7 text-center '>
       <img className='h-10 object-contain mx-auto mb-5' src="./src/yfhc_logo.png" alt="brand_logo_img" />
       <h3 className='text-xl pb-15'>How to setup a service</h3>
       <ul className='flex flex-col gap-5 w-150 mx-auto'>
@@ -189,7 +248,7 @@ export function HowItWorks(){
 export function Footer(){
 
   return(
-    <div className='bg-pink-7 text-sm text-white text-center py-5'>
+    <div className='bg-pink-7 text-sm text-white text-center py-5 w-150 sm:w-full'>
       <div>YourFamliyHouseCleanLLC</div>
       <div className='' >Site developed by Willian Sullivan-Smith</div>
     </div>
